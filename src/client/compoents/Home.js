c7bdcea2f0741ef9afe014cd592c3060f610fddf
vishwajeet singh chauhan <vishwajeet11104736@gmail.com>
2020-09-04 03:58:45 +0530

    webpack client configured

--- src/client/compoents/Home.js
+++ src/client/compoents/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>I'm the dope component.</div>;
+    return (
+        <div>
+            <div>I'm the dope component.</div>
+            <button onClick={() => console.log('Hi There!')}>Click</button>
+        </div>
+    );
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const React = require('react');
-const renderToString = require('react-dom/server').renderToString;
-const Home = require('./client/compoents/Home').default;
+import express from 'express';
+import renderer from './helpers/renderer';
 
 const app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    const content = renderToString(<Home/>);
-    res.send(content);
+    res.send(renderer());
 });
 app.listen(3000, () => {
-  console.log("Listening on prot 3000");
+  console.log("Listening on port 3000");
 });
