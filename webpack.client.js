421d67a769bb893dec1da3510cbc1b211098f9de
vishwajeet singh chauhan <vishwajeet11104736@gmail.com>
2020-09-06 23:31:10 +0530

    build file renamed

--- webpack.client.js
+++ webpack.client.js
@@ -9,7 +9,7 @@ const config = {
 
     // Tell webpack where to put the output file that is generated
     output: {
-        filename: 'bundle.js',
+        filename: 'index.js',
         path: path.resolve(__dirname, 'public')
     }
 };
--- webpack.server.js
+++ webpack.server.js
@@ -12,7 +12,7 @@ const config = {
 
     // Tell webpack where to put the output file that is generated
     output: {
-        filename: 'bundle.js',
+        filename: 'index.js',
         path: path.resolve(__dirname, 'build')
     },
 
